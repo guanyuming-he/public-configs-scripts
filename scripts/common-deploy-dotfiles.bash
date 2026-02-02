da0c66972dff76f46dbe425d36a0e044faccf1bf
Guanyuming He <guanyuminghe@proton.me>
2026-02-02 14:28:11 +0800

    separate deploy-dotfiles into functions.

---
+++ scripts/common-deploy-dotfiles.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Common functions used by clone-gitrepos.bash only.
+# 
+# MUST BE SOURCED AFTER THE VARS ARE DEFINES:
+# TOPICS, BASE_URIS, DEST, AND CWD.
+
+# $1: dir $2: file name
+install_file() {
+	if ! ( [[ $1 ]] && [[ $2 ]] ); then 
+		echo "can't install empty file or dir"
+		return 1
+	fi
+
+	local src=../dotfiles/$1/$2
+	if ! [[ -f ${src} ]]; then
+		echo "src dot file does not exist!!"
+		return 1
+	fi
+
+	local dest=${HOME}/$1/$2
+	if [[ -f ${dest} ]]; then
+		rm ${dest}
+	fi
+
+	ln ${src} ${dest} && echo "installed $2 to ${HOME}/$1"
+	return
+}
+
+main() {
+	for (( i=0 ; i < ${#DIRS[@]} ; i=$(($i+1)) )); do
+		dir=${DIRS[$i]}
+		[[ -d ${HOME}/${dir} ]] || mkdir ${dir}
+		declare -n files=DOTFILES_$i
+
+		for f in "${files[@]}"; do 
+			install_file ${dir} $f || echo "Could not install $f to ${dir}"
+		done
+	done
+}
--- scripts/deploy-dotfiles.bash
+++ scripts/deploy-dotfiles.bash
@@ -36,40 +36,9 @@ readonly -a DOTFILES_2=(
 ###############################################################################
 # Functions
 ###############################################################################
-
-# $1: dir $2: file name
-install_file() {
-	if ! ( [[ $1 ]] && [[ $2 ]] ); then 
-		echo "can't install empty file or dir"
-		return 1
-	fi
-
-	local src=../dotfiles/$1/$2
-	if ! [[ -f ${src} ]]; then
-		echo "src dot file does not exist!!"
-		return 1
-	fi
-
-	local dest=${HOME}/$1/$2
-	if [[ -f ${dest} ]]; then
-		rm ${dest}
-	fi
-
-	ln ${src} ${dest} && echo "installed $2 to ${HOME}/$1"
-	return
-}
+. ./common-deploy-dotfiles.bash
 
 ###############################################################################
 # Main
 ###############################################################################
-
-for (( i=0 ; i < ${#DIRS[@]} ; i=$(($i+1)) )); do
-	dir=${DIRS[$i]}
-	[[ -d ${HOME}/${dir} ]] || mkdir ${dir}
-	declare -n files=DOTFILES_$i
-
-	for f in "${files[@]}"; do 
-		install_file ${dir} $f || echo "Could not install $f to ${dir}"
-	done
-done
-
+main
